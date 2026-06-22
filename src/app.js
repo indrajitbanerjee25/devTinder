9751bf1c04a228dbc20e9bc57492d40dc5791d22
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-06-22 23:07:21 +0530

    Exploring get post and delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
+// This will only get call to "/user"
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Indrajit", lastName: "Banerjee" });
+});
+
+app.post("/user", (req, res) => {
+  //logig to update data in DB
+  res.send("Data Successfully save in DB");
+});
+
+app.delete("/user", (req, res) => {
+  //Delete logic for user delete
+  res.send("Data Successfully Delete from DB");
+});
+
+// This will match all HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello from the Dsshboard");
-});
-
-app.use("/", (req, res) => {
-  res.send("Welcome to Node");
 });
 
 app.listen(3000, () => {

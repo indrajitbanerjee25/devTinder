81f458507c035fe9fb84ecc9c6e222e9f9a9c783
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-06-23 22:51:30 +0530

    multiple route handler example

--- src/app.js
+++ src/app.js
@@ -2,26 +2,24 @@ const express = require("express");
 
 const app = express();
 
-// This will only get call to "/user"
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Indrajit", lastName: "Banerjee" });
-});
-
-app.post("/user", (req, res) => {
-  //logig to update data in DB
-  res.send("Data Successfully save in DB");
-});
-
-app.delete("/user", (req, res) => {
-  //Delete logic for user delete
-  res.send("Data Successfully Delete from DB");
-});
-
-// This will match all HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
-
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    res.send("Response from Route Handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling route user2 ");
+    res.send("Response from Route Handler 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling route user3 ");
+    res.send("Response from Route Handler 3");
+    next();
+  },
+);
 app.listen(3000, () => {
   console.log("Server listning on port 3000");
 });

92de20a8471c3fa36d0e1ce7ef4a2800c9b83ad2
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-06-24 23:41:11 +0530

    DB

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,1 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect("Indrajit");
-};
-
-module.exports = connectDB;
+//"Deleted"

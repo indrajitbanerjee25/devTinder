5c24e382d6c79669382cd5b26c50c21027ffdbdc
indrajitbanerjee25 <indrajit.banerjee25@gmail.com>
2026-06-24 23:34:28 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://nodedemoproject:iOBrjjNqmaaPWf6V@nodedemoproject.tsjxbpb.mongodb.net/",
+    "",
   );
 };
 

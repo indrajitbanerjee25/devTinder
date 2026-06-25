09dc6b0e4b294fd6c36537ee91e1fcc3ed450896
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-06-26 00:32:33 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -3,19 +3,38 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Juin",
-    lastName: "Banerjee",
-    emailId: "indrajit123@gmail.com",
-    password: "indrajit@123",
-    age: 33,
-  });
+  const user = new User(req.body);
   try {
     await user.save();
-    res.send("User Added Successfully !");
+    res.send("User Added Successfully !!");
   } catch (err) {
     res.status(400).send("Error Saving user");
+  }
+});
+
+app.get("/find", async (req, res) => {
+  const userEmailId = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmailId });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(user);
+  } catch (err) {
+    res.status(404).send("Something went wrong");
   }
 });
 

ee7e77b3561390e133cac87eafd5be4ba5724ae9
Indrajit Banerjee <indrajit.banerjee25@gmail.com>
2026-07-06 17:46:20 +0530

    Logout API building

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -24,6 +24,7 @@ authRouter.post("/signup", async (req, res) => {
     res.status(400).send("ERROR:" + err.message);
   }
 });
+
 authRouter.post("/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
@@ -44,6 +45,13 @@ authRouter.post("/login", async (req, res) => {
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
   }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("Logout Successful");
 });
 
 module.exports = authRouter;
